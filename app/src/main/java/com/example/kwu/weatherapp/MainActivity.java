e9a1be2a2023504e95e6c246e4779153800f99e7
Katherine Wu <kwu@newrelic.com>
2016-02-04 18:33:34 -0800

    Make a request for weather data

--- app/src/main/java/com/example/kwu/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/kwu/weatherapp/MainActivity.java
@@ -2,20 +2,48 @@ package com.example.kwu.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String API_KEY = "6e9c4bced2c2ac9edcfe5a2bc7337f6f";
-
     private static final String URL = "https://api.forecast.io/forecast/%s/%f,%f";
+    private static final String TAG = "MainActivity";
 
     private double latitude = 45.5200;
-
     private double longitude = -122.6819;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String forecastURL = String.format(URL, API_KEY, latitude, longitude);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastURL).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "Failed to get weather data", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "onResponse: " + response.body().string());
+            }
+        });
     }
 }
